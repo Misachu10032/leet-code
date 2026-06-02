71f47b7b8305e09d0dd69f84383d21084cb4b4ff
John Zhou <johnzhou.engineer@gmail.com>
2026-06-02 10:10:13 -0600

    189

--- 169-majprity-elements/solution.py
+++ 169-majprity-elements/solution.py
@@ -1,4 +1,28 @@
 class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        return nums[len(nums)//2]
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k %= len(nums)
+
+        for _ in range(k):
+            last=nums.pop()
+            nums.insert(0,last)
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k %= len(nums)
+
+        def reverse(l,r):
+            while l <r:
+                nums[l],nums[r]=nums[r],nums[l]
+                l +=1
+                r -=1
+
+        reverse(0,len(nums)-1)
+        reverse(0,k-1)
+        reverse(k,len(nums)-1)
+        
---
+++ 189-rotate-array/notes.md
@@ -0,0 +1,12 @@
+
+
+## Follow-ups / gotchas
+
+brute force: pop and insert k times pop last element is O(1). insert first is O(n). so O(k x n)
+
+similar to linked list. to rotate an array 
+reverse the whole thing. 
+reverse to k-1 index
+reverse k to end
+This is O(3 x n) ish
+
